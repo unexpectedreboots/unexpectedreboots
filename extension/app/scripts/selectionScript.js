ab3fe033ec16245627cbc845fb165e775e300945
Alec Maliwanag <amaliwanag@gmail.com>
2016-10-24 22:29:30 -0700

    Clean up drop down extension code.

--- extension/app/scripts/selectionScript.js
+++ extension/app/scripts/selectionScript.js
@@ -1,13 +1,6 @@
-// var highlighterApp = angular.module('highlighter', []);
-// document.addEventListener('load', function() {
-//   var html = document.querySelector('html');
-//   html.setAttribute('class', 'ng-app');
-//   var viewport = document.getElementById('viewport');
-//   viewport.setAttribute('ng-controller', 'MainController');
-// });
 document.addEventListener('mouseup',function(event) {
   var selectedText = window.getSelection().toString();
-  var parentElement = window.getSelection().anchorNode.parentElement;
+  var parentElement = window.getSelection().anchorNode.parentElement.;
   console.log(parentElement);
   if (selectedText.length) {
     console.log(Document.activeElement);
@@ -16,7 +9,7 @@ document.addEventListener('mouseup',function(event) {
     var textnode = document.createTextNode("test");
     console.log(node,'node')
     node.appendChild(textnode);
-    parentElement.insertAfter(node);
-  console.log(selectedText);
+    parentElement.appendChild(node);
+    console.log(selectedText);
   }
 });
