fcc434d74aea48e1473bd65687068b0b43736260
Christopher Tham <chriswt91@gmail.com>
2016-10-25 14:44:20 -0700

    Implement bcrypt hotfixes

--- db/users.js
+++ db/users.js
@@ -4,6 +4,7 @@ var CONNECTION = 'postgres://admin:rQUNyC8W9YT7ZZBW@localhost:5432/markable'
 var client = new pg.Client(CONNECTION);
 
 exports.insertUser = function(username, email, password, callback) {
+  console.log('db insert user called for: ', username, email, password);
   client.connect(function(err) {
     if (err) console.log(err);
 
@@ -16,7 +17,7 @@ exports.insertUser = function(username, email, password, callback) {
 
     function(err, rows) {
       if (err) {
-        callback('user creation failure');
+        callback('user creation failure', err);
         client.end(err);
       } else {
         callback('user creation success');
