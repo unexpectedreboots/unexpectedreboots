68020cf9be6daa5515a6d01aaa06ae8eb7e09643
Manjula Kumarasamy <manjulakumarasamy@Manjulas-MacBook-Air.local>
2016-10-25 09:18:55 -0700

    save to chrome local storage

--- extension/rebootBackground.js
+++ extension/rebootBackground.js
@@ -1,6 +1,29 @@
+String.prototype.hashCode = function() {
+  var hash = 0;
+  if (this.length === 0) {
+    return hash;
+  }
+  for (i = 0; i < this.length; i++) {
+    char = this.charCodeAt(i);
+    hash = ((hash << 5) - hash) + char;
+    hash = hash & hash; // Convert to 32bit integer
+  }
+  return hash;
+};
+
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
   console.log('in background');
   if (request.action === 'reboot-content') {
+    var objToStore = {};
+    objToStore.url = request.url;
+    objToStore.message = request.data;
+    objToStore.createdAt = new Date();
+    objToStore.position = [];
+    chrome.storage.local.set({hashCode: objToStore}, function() {
+      // Notify that we saved.
+      console.log('highlight saved');
+    });
+
     chrome.runtime.sendMessage(
     {action: 'reboot-background',
     data: request.data,
@@ -13,6 +36,3 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
 });
 
  
-
-
-
