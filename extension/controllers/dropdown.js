01e10311cb3a3017010ad3e4ec3717c27e7e2a54
Alec Maliwanag <amaliwanag@gmail.com>
2016-10-25 17:25:35 -0700

    Added extension form and created login in landing page for extension.

--- extension/controllers/dropdown.js
+++ extension/controllers/dropdown.js
@@ -1,13 +1,18 @@
 angular.module('dropdownController',[])
-.controller('dropdown',function($scope) {
-  $scope.selectedText = '';
-  $scope.getSelectionText = function() {
-    var text = "";
-    if (window.getSelection) {
-        $scope.selectedText = window.getSelection().toString();
-    } else if (document.selection && document.selection.type != "Control") {
-        $scope.selectedText = document.selection.createRange().text;
-    }
-    return text;
+.controller('dropdown',function($scope, $http, $location) {
+  $scope.signUp = function() {
+    console.log('this clicked!')
+    $location.path('/main');
+    // $http({ 
+    //   method: 'POST',
+    //   url: '/api/register',
+    //   data: {email: $scope.email, username: $scope.username, password: $scope.password},
+    //   headers:{'Content-Type': 'application/json'} 
+    // }).then(function(err,response) {
+    //   // if(response) {
+    //   //   //redirect
+        
+    //   // }
+    // });
   };
 });
