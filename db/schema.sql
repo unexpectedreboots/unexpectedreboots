cfafe86c0cf4abf06b73e9ba23c588ebba6d96bb
Christopher Tham <chriswt91@gmail.com>
2016-10-27 15:22:20 -0700

    Modify database to implement automatic timezone'd timestamps

--- db/schema.sql
+++ db/schema.sql
@@ -1,35 +1,39 @@
 CREATE TABLE users (
-  id        BIGSERIAL PRIMARY KEY,
+  id        BIGSERIAL   PRIMARY KEY,
   username  VARCHAR(20) NOT NULL,
   email     VARCHAR(64) NOT NULL,
-  password  VARCHAR(64) NOT NULL
+  password  VARCHAR(64) NOT NULL,
+  createdat TIMESTAMPTZ NOT NULL DEFAULT now()
 );
 
 CREATE TABLE groups (
-  id     BIGSERIAL PRIMARY KEY,
-  name   VARCHAR(32) NOT NULL,
-  owner  BIGSERIAL references users(id)
+  id        BIGSERIAL   PRIMARY KEY,
+  name      VARCHAR(32) NOT NULL,
+  owner     BIGSERIAL   references users(id),
+  createdat TIMESTAMPTZ NOT NULL DEFAULT now()
 );
 
 CREATE TABLE usersgroups (
-  userid  BIGSERIAL references users(id),
-  groupid BIGSERIAL references groups(id),
+  userid      BIGSERIAL   references users(id),
+  groupid     BIGSERIAL   references groups(id),
+  membersince TIMESTAMPTZ NOT NULL DEFAULT now()
   PRIMARY KEY (userid, groupid)
 );
 
 CREATE TABLE sites (
-  id    BIGSERIAL PRIMARY KEY,
-  url   VARCHAR(255) NOT NULL,
-  title VARCHAR(255) NOT NULL
+  id        BIGSERIAL    PRIMARY KEY,
+  url       VARCHAR(255) NOT NULL,
+  title     VARCHAR(255) NOT NULL,
 );
 
 CREATE TABLE markups (
-  id       BIGSERIAL PRIMARY KEY,
-  siteid   BIGSERIAL references sites(id),
-  authorid BIGSERIAL references users(id),
-  anchor   VARCHAR(255),
-  text     VARCHAR(2048) NOT NULL,
-  comment  VARCHAR(2048)
+  id        BIGSERIAL     PRIMARY KEY,
+  siteid    BIGSERIAL     references sites(id),
+  authorid  BIGSERIAL     references users(id),
+  anchor    VARCHAR(255),
+  text      VARCHAR(2048) NOT NULL,
+  comment   VARCHAR(2048),
+  createdat TIMESTAMPTZ   NOT NULL DEFAULT now()
 );
 
 CREATE TABLE markupsgroups (
@@ -38,8 +42,9 @@ CREATE TABLE markupsgroups (
 );
 
 CREATE TABLE sitesgroups (
-  groupid  BIGSERIAL references groups(id),
-  siteid   BIGSERIAL references sites(id),
-  sharedby BIGSERIAL references useers(id)
+  groupid  BIGSERIAL   references groups(id),
+  siteid   BIGSERIAL   references sites(id),
+  sharedby BIGSERIAL   references useers(id),
+  sharedat TIMESTAMPTZ NOT NULL DEFAULT now()
 );
 
