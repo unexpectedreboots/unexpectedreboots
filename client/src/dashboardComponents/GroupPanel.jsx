2b7d097428a58e181a6b95b5d2259d7a3c750935
Dylan Larrabee <dylan.r.larrabee@gmail.com>
2016-10-27 15:02:32 -0700

    Dynamically render a user's groups on homepage

--- client/src/dashboardComponents/GroupPanel.jsx
+++ client/src/dashboardComponents/GroupPanel.jsx
@@ -1,19 +1,50 @@
-var GroupPanel = (props) => {
+class GroupPanel extends React.Component {
 
-  return (
-    <div>
-      <AddGroup />
-      { props.groups.map(function(group) {
-        return (
-         <div>
-            <Group name={group} changeViewCb={props.changeViewCb} />
-          </div>
-      ); })
-      }  
-    </div>
-  );
+  constructor(props) {
+    super(props);
 
-};
+    this.state = {
+      groups: []
+    };
+  }
+
+  
+  componentDidMount() {
+    var context = this;
+
+    fetch('http://104.237.1.118:3000/test/users/groups', {
+      method: 'POST',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({username: '1'})
+    })
+    .then(function(res) {
+      return res.json();
+    })
+    .then(function(value) {
+      console.log(value);
+      context.setState({
+        groups: value
+      });
+    });
+
+    
+  }
+
+  render() {
+    return (
+      <div>
+        <AddGroup />
+        { this.state.groups.map((group) => {
+          return (
+            <div>
+              <Group group={group} changeViewCb={this.props.changeViewCb} />
+            </div>
+        ); })
+        }  
+      </div>
+    );
+  }
+}
 
 
 window.GroupPanel = GroupPanel;
