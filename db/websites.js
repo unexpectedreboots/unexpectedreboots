5c1ea206b2c5d73d1d602c48c7c15e5d53fa4bff
Christopher Tham <chriswt91@gmail.com>
2016-10-26 18:57:28 -0700

    Implement DB handler for website creation

--- db/websites.js
+++ db/websites.js
@@ -10,3 +10,29 @@ var CONFIG = {
 };
 
 var pool = new Pool(CONFIG);
+
+exports.createSite = function(url, title, callback) {
+  pool.query({
+    // check if website already exists
+    text: 'SELECT * FROM sites \
+      WHERE url = \'' + url + '\' \
+      AND title = \'' + title + '\''
+  },
+
+  function(err, rows) {
+    if (rows.rowCount > 0) {
+      callback('website already exists', null);
+    } else {
+
+      pool.query({
+        text: 'INSERT INTO sites(url, title) \
+          VALUES($1, $2)',
+        values: [url, title]
+      },
+
+      function(err2, rows2) {
+        err2 ? callback(err2, null) : callback(true, null);
+      });
+    }
+  });
+};
