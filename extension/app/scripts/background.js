876e37b5612ee9c1319f541ef7081182647bcc0d
Alec Maliwanag <amaliwanag@gmail.com>
2016-10-28 16:54:44 -0700

    Selection post now sends url, title, username, text.  Still need to fix comment.

--- extension/app/scripts/background.js
+++ extension/app/scripts/background.js
@@ -2,16 +2,28 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
   console.log('background script triggered');    
   var username = localStorage.getItem('username');
   var selection = request.selection;
-  $.ajax({
-    type: "POST",
-    url: 'http://104.237.1.118:3000/test/markups/create',
-    data: {
-      username: username,
-      anchor: selection
-    },
-    success: function() {
-      alert('success');
-    }
+  var url = '';
+  var title = '';
+  var text = request.text;
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    url = tabs[0].url;
+    title = tabs[0].title;
+  //need url, title, and text
+    $.ajax({
+      type: "POST",
+      url: 'http://104.237.1.118:3000/test/markups/create',
+      data: {
+        username: username,
+        anchor: selection,
+        url: url,
+        title: title,
+        text: request.text,
+        comment: null
+      },
+      success: function() {
+        alert('success');
+      }
+    })
   });
 });
 
