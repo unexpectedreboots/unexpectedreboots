1fa581da8b506240e942f1a90700945fda7c8d48
Christopher Tham <chriswt91@gmail.com>
2016-10-28 14:42:12 -0700

    Implement error handling for group creation

--- db/groups.js
+++ db/groups.js
@@ -27,39 +27,51 @@ exports.createGroup = function(groupName, owner, callback) {
   },
 
   function(err, rows) {
-    if (rows.rowCount > 0) {
-      callback('duplicate group name for specified user', null);
+    if (err) {
+      callback(err, null);
     } else {
-
-      pool.query({
-        text: 'SELECT u.id FROM users u \
-        WHERE u.username = \'' + owner + '\''
-      },
-
-      function(err2, rows2) {
-        var ownerID = rows2.rows[0].id;
+      if (rows.rowCount > 0) {
+        callback('duplicate group name for specified user', null);
+      } else {
 
         pool.query({
-          text: 'INSERT INTO groups(name, owner) \
-            VALUES($1, $2) \
-            RETURNING *',
-          values: [groupName, ownerID]
+          text: 'SELECT u.id FROM users u \
+          WHERE u.username = \'' + owner + '\''
         },
 
-        function(err3, rows3) {
-          var groupID = rows3.rows[0].id;
+        function(err2, rows2) {
+          if (err2) {
+            callback(err2, null);
+          } else {
+            var ownerID = rows2.rows[0].id;
 
-          pool.query({
-            text: 'INSERT INTO usersgroups(userid, groupid) \
-              VALUES ($1, $2)',
-            values: [ownerID, groupID]
-          }, 
+            pool.query({
+              text: 'INSERT INTO groups(name, owner) \
+                VALUES($1, $2) \
+                RETURNING *',
+              values: [groupName, ownerID]
+            },
 
-          function(err4, rows4) {
-            err4 ? callback(false, null) : callback(null, true);
-          });
+            function(err3, rows3) {
+              if (err3) {
+                callback(err3, null);
+              } else {
+                var groupID = rows3.rows[0].id;
+
+                pool.query({
+                  text: 'INSERT INTO usersgroups(userid, groupid) \
+                    VALUES ($1, $2)',
+                  values: [ownerID, groupID]
+                }, 
+
+                function(err4, rows4) {
+                  err4 ? callback(false, null) : callback(null, true);
+                });
+              }
+            });
+          }
         });
-      });
+      }
     }
   });
 };
