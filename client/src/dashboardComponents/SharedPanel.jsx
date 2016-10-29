daa6883a1b73fa463f191d70eca0d8c37751cdbd
Dylan Larrabee <dylan.r.larrabee@gmail.com>
2016-10-29 15:31:30 -0700

    Refactor sharedPannel to stateful component

--- client/src/dashboardComponents/SharedPanel.jsx
+++ client/src/dashboardComponents/SharedPanel.jsx
@@ -1,19 +1,37 @@
-var SharedPanel = (props) => {
+class SharedPanel extends React.Component {
 
-  return (
-    <div className='container col-sm-5 shared-panel'>
-      <div className='panel-title'>Shared Sites</div>
-        { props.sites.map(function(site) {
-          return (
-           <div className='entry'>
-              <SiteEntry site={site} />
-            </div>
-        ); })
-      }
-    </div>
-  );
+  constructor(props) {
+    super(props);
 
-};
+    this.state = {
+      sites: []
+    };
+  }
+
+  fetchGroupSites() {
+    //endpoint
+  }
+
+  conponentDidMount() {
+    this.fetchGroupSites();
+  }
+
+  render() {
+    return (
+      <div className='container col-sm-5 shared-panel'>
+        <div className='panel-title'>Shared Sites</div>
+          { this.props.sites.map(function(site) {  //change props to state on refactor
+            return (
+             <div className='entry'>
+                <SiteEntry site={site} />
+              </div>
+          ); })
+        }
+      </div>
+    );
+  }
+
+}
 
 
 window.SharedPanel = SharedPanel;
