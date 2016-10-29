4dae88bb3bcc1289d0d3bc471e43e8575751358e
Dylan Larrabee <dylan.r.larrabee@gmail.com>
2016-10-28 22:13:38 -0700

    Add welcome text on heading

--- client/src/dashboardComponents/Heading.jsx
+++ client/src/dashboardComponents/Heading.jsx
@@ -3,7 +3,9 @@ var Heading = (props) => {
   return ( 
     <div className='row heading'>
     <div>
-      <div className='col-sm-4 heading-title'>{props.title}</div> 
+      <div className='col-sm-4 heading-title'>{props.title} 
+        <div className='welcome' >Welcome back,</div>  
+      </div>
       <div className='col-sm-4 heading-bttn'>Logout</div>
       <div className='col-sm-4 heading-bttn' onClick={() => props.changeViewCb(event, 'home', null)}>Home</div>
     </div>
