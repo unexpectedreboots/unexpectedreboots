7d0f743e6d2d6ba865f6f4aaaf4ab37c307e0f4e
Dylan Larrabee <dylan.r.larrabee@gmail.com>
2016-10-29 15:27:40 -0700

    Refactor MarkupPanel to stateful component

--- client/src/dashboardComponents/MarkupPanel.jsx
+++ client/src/dashboardComponents/MarkupPanel.jsx
@@ -1,19 +1,40 @@
-var MarkupPanel = (props) => {
+class MarkupPanel extends React.Component {
 
-  return (
-    <div className='container col-sm-6 panel'>
-      <div className='panel-title'>Markup Panel</div>
-        { props.markups.map(function(markup) {
-          return (
-           <div className='entry'>
-              <MarkupEntry />
-            </div>
-        ); })
-      }
-    </div>
-  );
+  constructor(props) {
+    super(props);
 
-};
+    this.state = {
+      markups: []
+    };
+  }
+
+  fetchMarkupsFor(groupid) {
+    if (groupid === null) {
+    //use user markup endpoint 
+    } else {
+    //use group markup endpoint
+    }
+  }
+
+  componentDidMount() {
+    this.fetchMarkupsFor();
+  }
+
+  render() {
+    return (
+      <div className='container col-sm-6 panel'>
+        <div className='panel-title'>Markup Panel</div>
+          { this.props.markups.map(function(markup) { //change to the makkups held in 'state'
+            return (
+             <div className='entry'>
+                <MarkupEntry />
+              </div>
+          ); })
+        }
+      </div>
+    );
+  }
+}
 
 
 window.MarkupPanel = MarkupPanel;
