e4df930a9ca8d5651fe73ac1986cd2fbe738499f
Dylan Larrabee <dylan.r.larrabee@gmail.com>
2016-10-28 17:18:46 -0700

    Separate modal html intosepatate file

---
+++ client/src/modals.js
@@ -0,0 +1,24 @@
+$('body').append(
+
+//Ask what you want the name of the new group to be
+"<div class='remodal' data-remodal-id='modal'>"+
+  "<button data-remodal-action='close' class='remodal-close'></button>"+
+  "<h1>Group Name:</h1>"+
+  "<form>"+
+    "<br>"+
+   "<input class='groupName' type='text' name='groupName' />"+
+    "<br><br>"+
+    "<button data-remodal-action='confirm' class='remodal-confirm'>OK</button>"+
+  "</form>"+
+  "<br>"+
+"</div>"+
+
+//failed to make group a group because you already have one with that name
+"<div class='remodal' data-remodal-id='group-fail-modal'>"+
+  "<button data-remodal-action='close' class='remodal-close'></button>"+
+  "<h1>You alraedy have a group with that name  ¯\\_(ツ)_/¯</h1>"+
+  "<br>"+
+  "<button data-remodal-action='cancel' class='remodal-cancel'>OK ...</button>"+
+"</div>"
+
+);
